70c59e2f8543cdb036832e454d68ecbf6665b1af
Alexandre Souza <alexandre.souzafilho@hotmail.com>
2025-04-27 13:51:28 -0300

    Add colors for the calculator and create a shape for the app

--- calculator.py
+++ calculator.py
@@ -1,1 +1,25 @@
 ## file to start the calculator project
+
+from tkinter import *
+from tkinter import ttk
+
+
+#colors
+cor1 = "#1e1f1e" #black
+cor2 = "#feffff" #white 
+cor3 = "#38576b" #Blue/grey
+cor4 = "#ECEFF1" #grey
+cor5 = "#FFAB40" #orange
+
+
+
+janela = Tk()
+janela.title("Calculator")
+janela.geometry("400x570")
+janela.config(bg=cor3)
+
+frame_tela = Frame(janela, width=400, height=150)
+frame_tela.grid(row=0, column=0)
+
+
+janela.mainloop()
