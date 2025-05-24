a054734c7e38a266ee3f8ebaf21ce6a57e4da4b5
Alexandre Souza <alexandre.souzafilho@hotmail.com>
2025-05-24 09:47:10 -0300

    Add more layers and start add the buttons

--- calculator.py
+++ calculator.py
@@ -15,11 +15,25 @@ cor5 = "#FFAB40" #orange
 
 janela = Tk()
 janela.title("Calculator")
-janela.geometry("400x570")
-janela.config(bg=cor3)
+janela.geometry("235x318")
+janela.config(bg=cor1)
 
-frame_tela = Frame(janela, width=400, height=150)
+frame_tela = Frame(janela, width=235, height=50, bg=cor3)
 frame_tela.grid(row=0, column=0)
 
+
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
+
+# Buttons
+
+b_1 = Button(frame_corpo, text="C", width=11, height=2)
+b_1.place(x=0, y=0)
+
+b_2 = Button(frame_corpo, text="%", width=2, height=2)
+b_2.place(x=120, y=0)
+
+b_3 = Button(frame_corpo, text="/", width=2, height=2)
+b_3.place(x=180, y=0)
 
 janela.mainloop()
